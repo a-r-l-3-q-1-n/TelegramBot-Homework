5389613bcc147c8eecd5aa3763df36a33f34cde6
arl3q1n <arl3q1n.dev@gmail.com>
2023-12-28 02:19:16 +0200

    TelegramBot-Homework

--- Utils/Logger.py
+++ Utils/Logger.py
@@ -43,9 +43,6 @@ class Logger:
     def log_info(self, message):
         self.logger.info(message)
 
-    def log_warn(self, message):
-        self.logger.warning(message)
-
     def log_error(self, message):
         self.logger.error(message)
 
